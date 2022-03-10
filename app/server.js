0665f0241929df015f28ff3f5c398880c69362ec
nurelhhh <nurel_harsya@outlook.com>
2022-03-10 21:51:41 +0700

    First commit

---
+++ app/server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+// Constants
+const PORT = 5000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+//   res.send('Hello World');
+    res.sendFile(path.join(__dirname, './index.html'));
+    // res.status(200).json("Hello world");
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
